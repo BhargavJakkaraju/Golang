535391dc73fb8944d2ec1ed607c9ef72e3eed0c7
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-11-20 19:32:46 -0800

    basic web server structure

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,17 @@
 package main
 
-   import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-   func main() {
-       fmt.Println("Hello, World!")
-   }
+func main() {
+	r := gin.Default() //creates router with Logger and recovery middleware
+	
+	r.GET("/Ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Pong",
+		})
+	})
+
+	r.Run(":8080")
+}
