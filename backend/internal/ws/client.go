78eeb2949a44b5c477dc0b31540233da77f10763
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-12-04 18:41:14 -0800

    added loop to handle connection and handle invalid message parsing

--- backend/internal/ws/client.go
+++ backend/internal/ws/client.go
@@ -26,10 +26,41 @@ type Client struct {
 
 //readPump handles incoming requests from the websocket
 func (c *Client) readPump() {
+	//handles client disconnect
 	defer func() {
 		c.hub.unregister <- c
 		c.conn.Close()
 		log.Printf("Client %s disconnected from class %s", c.userID, c.classID)
 	} ()
+
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	//infinite loop that can handle messages on the same websocket connection
+	for {
+		_, messageBytes, err := c.conn.ReadMessage()
+
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Println("Websocket err for user %s: %s", c.userID, err)
+			}
+			break
+		}
+
+		//parsing the message
+		var msg Message
+		if err := json.Unmarshal(messageBytes, &msg); err != nil {
+			continue
+		}
+
+		if msg.ClassID != c.classID { continue}
+		if msg.UserID != c.userID{ continue}
+
+	}
+	
 }
 
